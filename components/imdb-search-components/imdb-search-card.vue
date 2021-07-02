72932e0de42813d61c64deb3063f75cc4828119b
oguzhankidik <kdk.oguzhan@gmail.com>
2021-07-02 14:02:27 +0300

    search

---
+++ components/imdb-search-components/imdb-search-card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img class="card-img-top" :src="selectedItem.Poster" alt="Poster can't be found.">
+    <div class="card-body">
+      <h5 class="card-title">{{ selectedItem.Title }}</h5>
+      <p class="card-text">
+        Release Date : {{detailedInfo.Released}}<br>
+        Director : {{detailedInfo.Director}}<br>
+        Actors : {{detailedInfo.Actors}}
+      </p>
+
+      <NuxtLink to="/filters">asfsa</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "selectedItem",
+  data() {
+    return {
+      detailedInfo:[],
+      apiKey: '&apikey=6448bbcf',
+      url:'http://www.omdbapi.com/?'
+    }
+  },
+  created() {
+   this.detailInfo()
+  },
+  props:{
+    selectedItem:{
+      type:Object,
+      default:()=>{}
+    }
+  },
+  methods:{
+    async getData(newUrl) {
+      const response = await axios.get(newUrl);
+      this.detailedInfo = response.data
+
+    },
+
+    detailInfo(){
+      this.url+='i='+this.selectedItem.imdbID+this.apiKey
+      this.getData(this.url)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/imdbsearch.vue
+++ pages/imdbsearch.vue
@@ -1,52 +1,83 @@
 <template>
-<div>
-{{mountains}}
+  <div>
+    <v-row>
+      <v-col class="col-sm-12 col-lg-4 col-md-6 col-12">
+    <v-text-field
+      outlined
+      v-model="searchParams.s"
+      label="enter title"
+    ></v-text-field>
+      </v-col>
+      <v-col class="col-sm-12 col-lg-4 col-md-6 col-12">
+    <v-text-field
+      outlined
+      v-model="searchParams.y"
+      label="enter year"
+    ></v-text-field>
+      </v-col>
+      <v-col class="col-sm-12 col-lg-4 col-md-6 col-12">
+    <v-select
+      outlined
+      v-model="searchParams.type"
+      :items="typeOption"
+      label="enter type"
+    >
+    </v-select>
+      </v-col>
+    </v-row>
+    <v-btn
+      rounded
+      @click="clickSearch"
+    >Search
+    </v-btn>
 
-  <v-text-field
-    v-model="filters.title"
-    label="enter title"
-  ></v-text-field>
-  <v-text-field
-    v-model="filters.year"
-    label="enter year"
-  ></v-text-field>
-  <v-btn
-  @click="fetch"
-  >bas</v-btn>
-
-</div>
+    <div class="row mt-4">
+      {{imdbResults.Error}}
+      <div v-for="(item,index) in imdbResults.Search" class="col" :key="index">
+        <imdb-search-card :selectedItem="item"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
+
+
 import axios from "axios";
-import filters from "~/pages/filters";
+
+import imdbSearchCard from "~/components/imdb-search-components/imdb-search-card";
 
 export default {
+  components: {imdbSearchCard},
   data() {
     return {
-      posts:[],
-      filters:['t','i','t'],
-      apiKey:'6448bbcf',
-      mountains:[],
-      yeniurl:'',
-      url:'https://www.omdbapi.com/?apikey=6448bbcf'
+      imdbResults: [],
+      apiKey: '&apikey=6448bbcf',
+      typeOption: [
+        "movie", "series", "short"
+      ],
+      searchParams: { 's': '', 'y': '', 'type': '' },
+      ret:[],
+      finalUrl:'https://omdbapi.com/?'
     }
   },
 
-
-  methods:{
-
-    async fetch() {
-      for (const item of this.filters) {
-        this.yeniurl='https://www.omdbapi.com/?'+item+'=' +'title'+'&apikey='+this.apiKey
-        this.mountains = await fetch(
-          this.yeniurl
-        ).then(res => res.json())
-      }
-
+  methods: {
+    async getData(newUrl) {
+      const response = await axios.get(newUrl);
+      this.imdbResults = response.data
 
     },
+
+    clickSearch() {
+      this.ret=[]
+      this.finalUrl='https://omdbapi.com/?'
+      for (let d in this.searchParams)
+        this.ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(this.searchParams[d]));
+      this.finalUrl+=this.ret.join('&')+this.apiKey
+      this.getData(this.finalUrl)
+    }
   }
 }
 </script>
